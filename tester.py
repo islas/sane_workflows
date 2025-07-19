6595f9f4e18c935ce096637f360cbbceadc83a69
Anthony Islas <aislas@ucar.edu>
2025-07-18 17:11:07 -0700

    Update demo script to use orchestrator

--- tester.py
+++ tester.py
@@ -3,6 +3,7 @@
 
 import dag
 from Action import Action
+from Orchestrator import Orchestrator
 
 graph = dag.DAG()
 
@@ -20,7 +21,7 @@ i = Action( "i" )
 j = Action( "j" )
 k = Action( "k" )
 l = Action( "l" )
-actions = [ a, b, c, d, e, f, g, h, h, i, j, k, l ]
+actions = [ a, b, c, d, e, f, g, h, i, j, k, l ]
 for act in actions:
   act.verbose_ = True
   act.set_config(
@@ -30,35 +31,53 @@ for act in actions:
     }
   )
 
+b.add_dependencies( "a" )
+c.add_dependencies( "a" )
+d.add_dependencies( "a", "b", "c" )
 
-graph.add_edge( a, b )
-graph.add_edge( a, c )
-graph.add_edge( c, d )
-graph.add_edge( b, d )
-graph.add_edge( a, d )
+f.add_dependencies( "e" )
+g.add_dependencies( "e" )
+h.add_dependencies( "e" )
+i.add_dependencies( "f", "h" )
+j.add_dependencies( "g", "h" )
+k.add_dependencies( "j" )
+l.add_dependencies( "k" )
 
-graph.add_edge( e, f )
-graph.add_edge( e, g )
-graph.add_edge( e, h )
-graph.add_edge( h, i )
-graph.add_edge( h, j )
-graph.add_edge( g, j )
-graph.add_edge( f, i )
-graph.add_edge( j, k )
-graph.add_edge( k, l )
 
-print( graph.topological_sort() )
+# graph.add_edge( a, b )
+# graph.add_edge( a, c )
+# graph.add_edge( c, d )
+# graph.add_edge( b, d )
+# graph.add_edge( a, d )
 
-print( graph.traversal_to( [ d, l ] ) )
-traversal_list = graph.traversal_list( [d, l] )
-print( "traversal_list" )
-print( traversal_list )
+# graph.add_edge( e, f )
+# graph.add_edge( e, g )
+# graph.add_edge( e, h )
+# graph.add_edge( h, i )
+# graph.add_edge( h, j )
+# graph.add_edge( g, j )
+# graph.add_edge( f, i )
+# graph.add_edge( j, k )
+# graph.add_edge( k, l )
 
-while len( traversal_list ) > 0:
-  next_nodes = graph.get_next_nodes( traversal_list )
-  print( f"Doing nodes : {next_nodes}")
-  for node in next_nodes:
-    node.launch()
-    graph.node_complete( node, traversal_list )
+# print( graph.topological_sort() )
 
+# print( graph.traversal_to( [ d, l ] ) )
+# traversal_list = graph.traversal_list( [d, l] )
+# print( "traversal_list" )
+# print( traversal_list )
+
+# while len( traversal_list ) > 0:
+#   next_nodes = graph.get_next_nodes( traversal_list )
+#   print( f"Doing nodes : {next_nodes}")
+#   for node in next_nodes:
+#     node.launch()
+#     graph.node_complete( node, traversal_list )
+
+orch = Orchestrator()
+
+for action in actions:
+  orch.add_action( action )
+
+orch.run_actions( ["d", "l" ] )
 print( "Finished" )
