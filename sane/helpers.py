82b9d75a6a31438d60dd48965ebbc48cdff28d68
Anthony Islas <aislas@ucar.edu>
2025-12-04 14:09:03 -0700

    Helper doc decorator

---
+++ sane/helpers.py
@@ -0,0 +1,26 @@
+import functools
+
+def copydoc( *funcs, append=True ):
+  """Copy docstrings from funcs"""
+  def mod_doc( orig ):
+    prefix = "\n**From** :py:meth:`{function}`:\n\n"
+
+    orig_prefix = prefix.format( function=orig.__qualname__ )
+    if not hasattr( orig, "__orig_doc__" ):
+      orig.__orig_doc__ = orig.__doc__
+
+    if orig.__orig_doc__ is not None and orig_prefix not in orig.__doc__:
+      orig.__doc__ = orig_prefix + orig.__orig_doc__
+
+    full_doc = orig.__doc__
+    
+    for f in funcs:
+      if not hasattr( f, "__orig_doc__" ):
+        f.__orig_doc__ = f.__doc__
+
+      if f.__orig_doc__ is not None:
+        f_doc = prefix.format( function=f.__qualname__ ) + f.__orig_doc__
+        full_doc = ( full_doc if append else f_doc ) + ( f_doc if append else full_doc )
+    orig.__doc__ = full_doc
+    return orig
+  return mod_doc
