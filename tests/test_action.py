7b6b6e514eb5601297531023f87d9cc2097035bd
Anthony Islas <aislas@ucar.edu>
2026-06-08 17:03:16 -0700

    Add more complex dereference tests
    Test for edge cases of complex cycles and malformed but accepted strings

--- tests/test_action.py
+++ tests/test_action.py
@@ -128,7 +128,14 @@ class ActionTests( unittest.TestCase ):
     options = {
                 "environment" : "foobar",
                 "local"       : True,
-                "config"      : { "one" : 1, "two" : [2], "three" : { "foo" : 3 } },
+                "config"      :
+                { 
+                  "one" : 1,
+                  "two" : [2],
+                  "three" : { "foo" : 3 },
+                  "foobar" : "foo",
+                  "arr" : [ 3, 2, 1, 0 ]
+                },
                 "dependencies" :
                 {
                   "dep_action0" : "afterok",
@@ -180,9 +187,11 @@ class ActionTests( unittest.TestCase ):
                 {
                   "foo" : "${{ local }}",
                   "foobar" : "${noop}",
+                  "noop" : "${{ }}",
                   "moo" : [ "${{ working_directory}}", "${{ config.one }}", "${{ resources.gpus}}" ]
                 },
-                "boo" : "${{ config.two[0] }}"
+                "boo" : "${{ config.two[0] }}",
+                "moo" : "${{ config.two[ ${{ config.arr[ ${{ config.three.${{ config.foobar }} }} ] }} ] }}"
               }
     exp_dict = {
                 "foo" : "test",
@@ -191,9 +200,11 @@ class ActionTests( unittest.TestCase ):
                 {
                   "foo" : "True",
                   "foobar" : "${noop}",
+                  "noop" : "${{ }}",
                   "moo" : [ "./", "1", "999" ]
                 },
-                "boo" : "2"
+                "boo" : "2",
+                "moo" : "2"
               }
     out_dict = self.action.dereference( ref_dict )
     self.assertEqual( exp_dict, out_dict )
@@ -202,6 +213,68 @@ class ActionTests( unittest.TestCase ):
     self.action.config["foo"] = "${{ config.bar }}"
     self.action.config["bar"] = "1"
     ref_str = "${{config.foo}}"
+    exp_str = "1"
+    out_str = self.action.dereference_str( ref_str )
+    self.assertEqual( exp_str, out_str )
+
+  def test_action_dereference_detect_cycle( self ):
+    """Test the action's ability to detect cycles in attribute dereferencing"""
+    # Start with a sufficiently complex config
+    self.test_action_from_options()
+
+    # Modify action to self reference
+    self.action.config["single_cycle"] = {
+                                          "a" : "${{ config.single_cycle.b }}",
+                                          "b" : "${{ config.single_cycle.a }}"
+                                          }
+
+    ref_str = "${{ config.single_cycle.a }}"
+    exp_str = ref_str
+    out_str = self.action.dereference_str( ref_str )
+    self.assertEqual( exp_str, out_str )
+
+    self.action.config["quad_cycle"] = {
+                                          "a" : "${{ config.quad_cycle.b }}",
+                                          "b" : "${{ config.quad_cycle.c }}",
+                                          "c" : "${{ config.quad_cycle.d }}",
+                                          "d" : "${{ config.quad_cycle.a }}"
+                                          }
+
+    ref_str = "${{ config.quad_cycle.a }}"
+    exp_str = ref_str
+    out_str = self.action.dereference_str( ref_str )
+    self.assertEqual( exp_str, out_str )
+
+    self.action.config["enter_cycle"] = {
+                                          "a" : "${{ config.enter_cycle.b }}",
+                                          "b" : "${{ config.enter_cycle.c }}",
+                                          "c" : "${{ config.enter_cycle.d }}",
+                                          "d" : "${{ config.quad_cycle.b }}"
+                                        }
+    # Enter cycle at b, so expect b out
+    ref_str = "${{ config.enter_cycle.a }}"
+    exp_str = self.action.config["enter_cycle"]["d"]
+    out_str = self.action.dereference_str( ref_str )
+    self.assertEqual( exp_str, out_str )
+
+    # Cycle starts but does one loop to regularize the string so first err will
+    # be at next node
+    ref_str = "${{   config.single_cycle.a   }}"
+    exp_str = self.action.config["single_cycle"]["a"] #goes to next node
+    out_str = self.action.dereference_str( ref_str )
+    self.assertEqual( exp_str, out_str )
+
+  def test_action_dereference_warn_simplified_re( self ):
+    """Test the action's ability to detect malformed reference due to simplified regex"""
+    # Start with a sufficiently complex config
+    self.test_action_from_options()
+
+    # The simplification of the regex to be a repeating pattern with an optional
+    # '.' (key subscript) at the end allows a ref string to end with a '.'
+    self.action.config["foo"] = 1
+
+    # Detect if this is caught
+    ref_str = "${{config.foo.}}"
     exp_str = "1"
     out_str = self.action.dereference_str( ref_str )
     self.assertEqual( exp_str, out_str )
