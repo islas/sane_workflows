2167f9d4c8afa0dd2a9d63cc6f4c0aa8ab58357c
Anthony Islas <aislas@ucar.edu>
2026-06-12 15:35:56 -0700

    Create mock HPCHost class within SANE
    Use mock HPC system to create a very rudimentary `HPCHost` class.
    
    It does not do any error checking or resource management and instead
    focuses on behavior of deferred execution.

--- tests/test_hpc_host.py
+++ tests/test_hpc_host.py
@@ -1,7 +1,75 @@
 import unittest
+import subprocess
+import os
+import re
+import sys
 
 import sane
 from sane.helpers import recursive_update
+
+
+class MockHPC( sane.HPCHost ):
+  def __init__( self, name, aliases=[] ):
+    super().__init__(name, aliases)
+    self.account = "foobar"
+    self.runner_dir = os.path.dirname( __file__ ) + "/mock_hpc"
+    self.runner = None
+    self._state_cmd  = f"cat {self.runner_dir}/complete/{0}"
+    self._status_cmd = self._state_cmd
+    self._submit_cmd = f"{self.runner_dir}/submit.sh"
+    self._delay_sec = 0.1
+    self._cmd_delim = "--"
+    self._submit_format["dependency"] = "{0}"
+
+  def pre_run_actions( self, actions ):
+    # Launch mock runner with 1 second delay
+    self.runner = subprocess.Popen( [ self.runner_dir + "/run.sh", "0.1" ] )
+    return super().pre_run_actions( actions )
+
+  def post_run_actions( self, actions ):
+    # Complete post run processing
+    super().post_run_actions(actions)
+
+    # Kill the mock runner
+    with open( self.runner_dir + "/kill", "w" ):
+      pass
+
+    self.runner.wait()
+
+  def check_job_complete( self, job_id, retval, status ):
+    if retval != 0:
+      return False
+
+    return True
+
+  def check_job_status( self, job_id, retval, status ):
+    complete = self.check_job_complete( job_id, retval, status )
+    if not complete:
+      return False
+
+    return int(status) == 0
+
+  def extract_job_id( self, content ):
+    found = re.match( r"^Launching job (\d+)", content )
+    if found is None:
+      self.log( "No job id found in output from job submission", level=40 )
+      raise RuntimeError( "No job id found" )
+    else:
+      return int( found.group( 1 ) )
+
+  def submit_args( self, resource_dict, requestor_name ):
+    return {}, "none"
+
+  # These are generally complex and I don't feel like writing them so just don't
+  # over-subscribe your local system, ok?
+  def nonlocal_acquire_resources( self, resource_dict, requestor ):
+    return True
+
+  def nonlocal_release_resources(self, resource_dict, requestor):
+    return True
+
+  def nonlocal_resources_available(self, resource_dict, requestor, log=True):
+    return True
 
 class HPCHostTests( unittest.TestCase ):
   def setUp( self ):
