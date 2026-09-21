ddfa1111fb2fa61400951774eead10e8f10a9522
Anthony Islas <aislas@ucar.edu>
2026-09-21 11:11:14 -0700

    Fix error where logs no longer worked due to overwriting subparser variable

--- bin/sane_view.py
+++ bin/sane_view.py
@@ -255,7 +255,7 @@ def get_parser():
   status  = subparsers.add_parser( "status",  help="View action status", parents=[base] )
   state   = subparsers.add_parser( "state",   help="View action state", parents=[base] )
   logs    = subparsers.add_parser( "logs",    help="View action logs", parents=[base] )
-  logs    = subparsers.add_parser( "summary", help="View workflow summary", parents=[base] )
+  summary = subparsers.add_parser( "summary", help="View workflow summary", parents=[base] )
   usage.add_argument(
                       "-a", "--arrows",
                       action="store_true",
